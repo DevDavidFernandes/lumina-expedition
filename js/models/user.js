42d767c3fb67961f5aee546afab7a1cd0600ff88
David Fernandes Ramos <davidfernandesdev@gmail.com>
2026-06-23 15:12:07 -0300

    estrutura e css do login terminados

---
+++ js/models/user.js
@@ -0,0 +1,11 @@
+const User = {
+  id: "", // string única gerada na criação
+  nome: "",
+  email: "",
+  senha: "",
+  xp: 0,
+  chroma: 0,
+  avatar: "", // item equipado atualmente
+  inventario: [], // itens comprados com chroma
+  dataCriacao: "", // ISO string: new Date().toISOString()
+};
